b059b96189cfab3b12654f6aeaa7712608073e0e
douzi <249078602@qq.com>
2021-04-15 09:19:08 +0800

    dz
    提交远程仓库

---
+++ src/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DlcIndex',
+    components: { ComponentName },
+    directives: { DirectiveName },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
